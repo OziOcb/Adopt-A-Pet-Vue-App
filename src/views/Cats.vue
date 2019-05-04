440e962ef292dcc8ece24505e37fcc386bff4911
”Ozi <31310132+OziOcb@users.noreply.github.com>
2019-05-04 08:24:23 +0100

    Add router link for each dog

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -3,7 +3,7 @@
     <h1>Cats for Adoption</h1>
     <b-table striped hover :items="cats">
       <template slot="name" slot-scope="data">
-        <router-link :to="`/pet/${data.index}`">
+        <router-link :to="`/pets/${data.index}`">
           {{ data.value }}
         </router-link>
       </template>
